0b25471161e45911db8c02873486b323af19152d
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 11:09:17 +0530

    runjava

---
+++ run.java
@@ -0,0 +1,19 @@
+class Run{
+static byte [] ids={2,4,6,8,10,12,14,16,18,20};
+public static void main (String[] args)
+
+{
+
+System.out.println(ids[0]);
+System.out.println(ids[1]);
+System.out.println(ids[2]);
+System.out.println(ids[3]);
+System.out.println(ids[4]);
+System.out.println(ids[5]);
+System.out.println(ids[6]);
+System.out.println(ids[7]);
+System.out.println(ids[8]);
+System.out.println(ids[9]);
+}
+
+}
